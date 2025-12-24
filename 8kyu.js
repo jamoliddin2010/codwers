a0aa59842410526b549f4c4f1cf99c186d67f86b
jamoliddin2010 <jamol20100603@gmail.com>
2025-12-24 07:21:28 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -32,18 +32,27 @@
 
 // javob
 
-function smash(words) {
-  let sentence = "";
+// function smash(words) {
+//   let sentence = "";
 
-  for (let i = 0; i < words.length; i++) {
-    sentence += words[i];
-    if (i !== words.length - 1) {
-      sentence += " ";
-    }
-  }
+//   for (let i = 0; i < words.length; i++) {
+//     sentence += words[i];
+//     if (i !== words.length - 1) {
+//       sentence += " ";
+//     }
+//   }
 
-  return sentence;
+//   return sentence;
+// }
+
+// savol////////////////////////////////////////////////
+// Fix the function I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number. Can you help me fix the function?
+
+
+
+
+
+// javob///
+function addFive(num) {
+  return num + 5;
 }
-
-
-
