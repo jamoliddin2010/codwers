6443a2e234227afb8ee72421fd1dc9138f889bcc
jamoliddin2010 <jamol20100603@gmail.com>
2025-12-24 17:13:52 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -53,6 +53,21 @@
 
 
 // javob///
-function addFive(num) {
-  return num + 5;
-}
+// function addFive(num) {
+//   return num + 5;
+// }
+
+
+
+
+// savol
+
+// HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
+
+// The sorting should NOT be case sensitive
+
+
+
+// javob
+
+let sorter = (textbooks) => textbooks.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : -1)
