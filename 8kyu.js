951a9350b64fce7b33400c90fbcd4336a70e2b38
jamoliddin2010 <jamol20100603@gmail.com>
2026-01-07 17:40:24 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -157,12 +157,31 @@
 
 
 
-function consecutive(arr) {
- let min =Math.min(...arr)
- let max =Math.max(...arr)
- let newarr = []
-  for(let i= min; i <= max; i++) newarr.push(i)
-  return (newarr.length - arr.length);
+// function consecutive(arr) {
+//  let min =Math.min(...arr)
+//  let max =Math.max(...arr)
+//  let newarr = []
+//   for(let i= min; i <= max; i++) newarr.push(i)
+//   re
+
+// savol
+// Order People by age Using Arrow Function
+// Sort and Order people by their age using Arrow Functions
+// Task
+// Your task is to order a list containg people objects by age using the new Javascript Arrow Functions
+// Input
+// Input will be a valid array with People objects containing an Age and Name
+// Output
+// Output will be a valid sorted array with People objects sorted by Age in ascending order
+
+
+
+
+
+
+
+var OrderPeople = function(people){
+  return people.sort((a,b)=>a.age -b.age ); 
 }
 
 
