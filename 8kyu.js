02e5a7afd7316ea62635d06d9f83f42b9c48147f
jamoliddin2010 <jamol20100603@gmail.com>
2026-01-13 18:30:08 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -338,27 +338,46 @@
 
 
 
-let results = [
-  { name: "Ali", score: 78 },
-  { name: "Vali", score: 92 },
-  { name: "Hasan", score: 55 },
-  { name: "Husan", score: 66 }
-];
-let sum = results.filter((obj)=> obj.score > 60)
-  sum.sort((a, b)=> b.score - a.score)
-for (let s of sum) {
-  if (s.score >= 90) s.grade = "A";
- else if(s.score >= 75) s.grade = "B"
- else s.grade = "C"
+// let results = [
+//   { name: "Ali", score: 78 },
+//   { name: "Vali", score: 92 },
+//   { name: "Hasan", score: 55 },
+//   { name: "Husan", score: 66 }
+// ];
+// let sum = results.filter((obj)=> obj.score > 60)
+//   sum.sort((a, b)=> b.score - a.score)
+// for (let s of sum) {
+//   if (s.score >= 90) s.grade = "A";
+//  else if(s.score >= 75) s.grade = "B"
+//  else s.grade = "C"
+// }
+
+// let res = sum.reduce((acc, curr) => acc + curr.score, 0) / sum.length
+// console.log(sum);
+// console.log(+res.toFixed(2));
+
+
+// savol
+
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+// The geese are any strings in the following array, which is pre-populated in your solution:
+
+//   ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+// For example, if this array were passed as an argument:
+
+//  ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+// Your function would return the following array:
+
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+
+
+
+function gooseFilter(birds) {
+  let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter(bird => !geese.includes(bird));
 }
-
-let res = sum.reduce((acc, curr) => acc + curr.score, 0) / sum.length
-console.log(sum);
-console.log(+res.toFixed(2));
-
-
-
-
 
 
 
