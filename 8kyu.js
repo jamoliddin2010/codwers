054307da69911869c58e0748f4291f0e99603d4b
jamoliddin2010 <jamol20100603@gmail.com>
2026-01-15 17:24:40 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -374,9 +374,39 @@
 
 
 
-function gooseFilter(birds) {
-  let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-  return birds.filter(bird => !geese.includes(bird));
+// function gooseFilter(birds) {
+//   let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+//   return birds.filter(bird => !geese.includes(bird));
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// savol
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+
+
+
+
+
+// javob
+function filter_list(l) {
+  return l.filter(x => typeof x === 'number');
 }
 
 
