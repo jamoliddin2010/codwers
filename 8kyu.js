d0aed6280eb15a2b3d27bccc8cd31a1f17889e7f
jamoliddin2010 <jamol20100603@gmail.com>
2026-01-30 18:48:35 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -798,18 +798,33 @@ function invert(array) {
 
 
 
-function whatNumberIsIt(n) {
-  if (Number.isNaN(n)) return "Input number is Number.NaN";
-  if (n === Number.MAX_VALUE) return "Input number is Number.MAX_VALUE";
-  if (n === Number.MIN_VALUE) return "Input number is Number.MIN_VALUE";
-  if (n === Number.POSITIVE_INFINITY) return "Input number is Number.POSITIVE_INFINITY";
-  if (n === Number.NEGATIVE_INFINITY) return "Input number is Number.NEGATIVE_INFINITY";
-  return "Input number is " + n;
+// function whatNumberIsIt(n) {
+//   if (Number.isNaN(n)) return "Input number is Number.NaN";
+//   if (n === Number.MAX_VALUE) return "Input number is Number.MAX_VALUE";
+//   if (n === Number.MIN_VALUE) return "Input number is Number.MIN_VALUE";
+//   if (n === Number.POSITIVE_INFINITY) return "Input number is Number.POSITIVE_INFINITY";
+//   if (n === Number.NEGATIVE_INFINITY) return "Input number is Number.NEGATIVE_INFINITY";
+//   return "Input number is " + n;
+// }
+
+// savol
+// Write a function that gets a sequence and value and returns true/false depending on whether the variable exists in a multidimentional sequence.
+
+// Example:
+
+// ['a','b',['c','d',['e']]] , 'e' --> true
+// ['a','b',['c','d',['e']]] , 'a' --> true
+// ['a','b',['c','d',['e']]] , 'f' --> false
+
+
+let locate = (arr, value)=> {
+  const flatArr = arr.flat(Infinity);
+
+  if(flatArr.includes(value)) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
-
-
-
-
 
 
