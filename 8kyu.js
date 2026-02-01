97afcc675c3055a0cfef01e174a8be32f1eec4dc
jamoliddin2010 <jamol20100603@gmail.com>
2026-02-01 20:20:03 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -817,14 +817,53 @@ function invert(array) {
 // ['a','b',['c','d',['e']]] , 'f' --> false
 
 
-let locate = (arr, value)=> {
-  const flatArr = arr.flat(Infinity);
+// let locate = (arr, value)=> {
+//   const flatArr = arr.flat(Infinity);
 
-  if(flatArr.includes(value)) {
-    return true;
-  } else {
-    return false;
+//   if(flatArr.includes(value)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
+
+
+// savol
+// Write a function loopArr that loops array in a specified direction by some number of steps.
+
+// By "looping array" it means removing elements from start and adding them to end of array one-by-one (if direction is "left") or removing from end and adding them to start one by-one (if direction is "right").
+
+// Function should accept three arguments:
+
+// array - non-empty array of elements of any type;
+// direction - 'left' or 'right' - tells how to loop array;
+// steps - number of steps to loop array (less or equal to array size);
+// Examples:
+
+// loopArr([1, 5, 87, 45, 8, 8], 'left', 2);
+// should produce result: [87, 45, 8, 8, 1, 5]
+
+// loopArr([1, 5, 87, 45, 8, 8], 'right', 2);
+// should produce result: [8, 8, 1, 5, 87, 45]
+
+
+
+
+
+function loopArr(arr, direction, steps) {
+  let res = arr.slice(); 
+
+  for (let i = 0; i < steps; i++) {
+    if (direction === 'left') {
+      res.push(res.shift());
+    } else if (direction === 'right') {
+      res.unshift(res.pop());
+    }
   }
+
+  return res;
 }
 
 
