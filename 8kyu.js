310a104961dbf1f09fd00f56f274d39e2df49a4e
jamoliddin2010 <jamol20100603@gmail.com>
2026-02-07 23:22:51 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -901,14 +901,25 @@ function invert(array) {
 // Example : n=5 --> [5,4,3,2,1]
 
 
-function reverseSeq(n) {
-  const result = [];
-  for (let i = n; i >= 1; i++) {
-    result.push(i);
+// function reverseSeq(n) {
+//   const result = [];
+//   for (let i = n; i >= 1; i++) {
+//     result.push(i);
+//   }
+//   return result;
+// }
+// Implement a function that takes two numbers m and n and returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+// Ex.
+
+// (3, 5.0) --> [5.0, 10.0, 15.0]
+
+function multiples(m, n) {
+  let result = [];
+  for (let i = 1; i <= m; i++) {
+    result.push(i * n);
   }
   return result;
 }
-
-
 
 
