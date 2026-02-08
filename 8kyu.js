a1933f26f720c7321a8f369eb726044aa1c1192c
jamoliddin2010 <jamol20100603@gmail.com>
2026-02-08 23:45:30 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -914,12 +914,33 @@ function invert(array) {
 
 // (3, 5.0) --> [5.0, 10.0, 15.0]
 
-function multiples(m, n) {
-  let result = [];
-  for (let i = 1; i <= m; i++) {
-    result.push(i * n);
-  }
-  return result;
+// function multiples(m, n) {
+//   let result = [];
+//   for (let i = 1; i <= m; i++) {
+//     result.push(i * n);
+//   }
+//   return result;
+// }
+Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor (due to superstition).
+
+// Write a function that given a floor in the american system returns the floor in the european system.
+
+// With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers move down to take their place. In case of above 13, they move down by two because there are two omitted numbers below them.
+
+// Basements (negatives) stay the same as the universal level.
+
+// More information here
+
+// Examples
+// 1  =>  0 
+// 0  =>  0
+// 5  =>  4
+// 15  =>  13
+// -3  =>  -3
+
+
+function getRealFloor(n) {
+  if (n <= 0) return n;
+  if (n < 13) return n - 1;
+  return n - 2;
 }
-
-
