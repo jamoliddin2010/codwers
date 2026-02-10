f036bbb1e4a85484ddc580373c7b1aaf9c248fe6
jamoliddin2010 <jamol20100603@gmail.com>
2026-02-10 23:54:44 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -963,6 +963,39 @@ function invert(array) {
 
 
 
-function rowSumOddNumbers(n) {
-  return n ** 3;
+// function rowSumOddNumbers(n) {
+//   return n ** 3;
+// }
+
+// savol
+
+// Rick wants a faster way to get the product of the largest pair in an array. Your task is to create a performant solution to find the product of the largest two integers in a unique array of positive numbers.
+
+// All inputs will be valid.
+// Rick is only interested in solutions that are faster than his, which has a running time of O(n log n).
+// [2, 6, 3]                      => 18 = 6 * 3
+// [2, 1, 5, 0, 4, 3]             => 20 = 5 * 4
+// [7, 8, 9]                      => 72 = 8 * 9
+// [33, 231, 454, 11, 9, 99, 57]  => 104874 = 231 * 454
+
+
+
+
+
+
+
+function maxProduct(numbers) {
+  let max1 = 0;
+  let max2 = 0;
+
+  for (let num of numbers) {
+    if (num > max1) {
+      max2 = max1;
+      max1 = num;
+    } else if (num > max2) {
+      max2 = num;
+    }
+  }
+
+  return max1 * max2;
 }
