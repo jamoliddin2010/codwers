ec74fd9bd96478c346fb79bed2bc6ca7a3b301ba
jamoliddin2010 <jamol20100603@gmail.com>
2026-02-14 07:52:23 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -1055,7 +1055,45 @@ function invert(array) {
 // Description: You are given two interior angles (in degrees) of a triangle.  Write a function to return the 3rd.  Note: only positive integers will be tested. 
 
 
-function otherAngle(a, b) {
-  return 180 - (a + b);
+// function otherAngle(a, b) {
+//   return 180 - (a + b);
+// }
+
+
+
+
+// Linked lists are data structures composed of nested or chained objects, each containing a single value and a reference to the next object.
+
+// Here's an example of a list:
+
+// {value: 1, next: {value: 2, next: {value: 3, next: null}}}
+// Write a function listToArray (or list_to_array in Python) that converts a list to an array, like this:
+
+// [1, 2, 3]
+// Assume all inputs are valid lists with at least one value. For the purpose of simplicity, all values will be either numbers, strings, or Booleans.
+
+
+
+
+function listToArray(list) {
+  const result = [];
+  
+  while (list !== null) {
+    result.push(list.value);
+    list = list.next;
+  }
+  
+  return result;
 }
+
+
+
+
+
+
+
+
+
+
+
 
