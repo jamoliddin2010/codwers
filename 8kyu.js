f3a75cd78cc3faf00e61ee090b0b73251a12405f
jamoliddin2010 <jamol20100603@gmail.com>
2026-02-24 21:45:37 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -1258,10 +1258,10 @@ function invert(array) {
 // case	return
 // name equals owner	'Hello boss'
 // otherwise	'Hello guest'
-function greet(name, owner) {
-  if (name === owner) {
-    return "Hello boss";
-  } else {
-    return "Hello guest";
-  }
-}
+// function greet(name, owner) {
+//   if (name === owner) {
+//     return "Hello boss";
+//   } else {
+//     return "Hello guest";
+//   }
+// }
