772ea57c4ab97d92dd113e5956a99ab66114485a
jamoliddin2010 <jamol20100603@gmail.com>
2026-03-02 21:41:50 +0500

    coment

--- 8kyu.js
+++ 8kyu.js
@@ -1304,17 +1304,44 @@ function invert(array) {
 // solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
 // solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
 // More examples in test cases. Good luck!
-function solve(s) {
-  let lower = 0;
-  let upper = 0;
+// function solve(s) {
+//   let lower = 0;
+//   let upper = 0;
 
-  for (let char of s) {
-    if (char === char.toLowerCase()) {
-      lower++;
-    } else {
-      upper++;
+//   for (let char of s) {
+//     if (char === char.toLowerCase()) {
+//       lower++;
+//     } else {
+//       upper++;
+//     }
+//   }
+
+//   return upper > lower ? s.toUpperCase() : s.toLowerCase();
+// }
+
+// Given a number n, you should find a set of numbers for which the sum equals n. This set must consist exclusively of values that are a power of 2 (eg: 2^0 => 1, 2^1 => 2, 2^2 => 4, ...).
+
+// The function powers takes a single parameter, the number n, and should return an array of unique numbers.
+
+// Criteria
+// The function will always receive a valid input: any positive integer between 1 and the max integer value for your language (eg: for JavaScript this would be 9007199254740991 otherwise known as Number.MAX_SAFE_INTEGER).
+
+// The function should return an array of numbers that are a power of 2 (2^x = y).
+
+// Each member of the returned array should be unique. (eg: the valid answer for powers(2) is [2], not [1, 1])
+
+// Members should be sorted in ascending order (small -> large). (eg: the valid answer for powers(6) is [2, 4], not [4, 2])
+function powers(n) {
+  const result = [];
+  let power = 1;
+
+  while (n > 0) {
+    if (n % 2 === 1) {
+      result.push(power);
     }
+    n = Math.floor(n / 2);
+    power *= 2;
   }
 
-  return upper > lower ? s.toUpperCase() : s.toLowerCase();
+  return result;
 }
